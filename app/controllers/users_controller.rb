6262efc7faf86909749121c8b29ac6b6f4ff4908
Bruno Silva <brunosilvinosilva@gmail.com>
2020-03-03 13:06:42 +0000

    testing login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,15 @@
 class UsersController < ApplicationController
 
     def show 
-
+        @user = User.find(params[:id])
     end 
 
     def new 
-        
+        @user = User.new
     end 
 
     def create
-        @user = User.create(user_params)
-        return redirect_to controller: 'users', action: 'new' unless @user.save
-        session[:user_id] = @user.id
-        redirect_to controller: 'sessions', action: 'new'
+       
     end
     
 
@@ -27,6 +24,6 @@ class UsersController < ApplicationController
     private 
 
     def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        params.require(:user).permit(:name, :email, :password_digest, :password_confirmation)
     end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :bookings 
     has_many :performances, through: :bookings
 
-    has_secure_password
+  
 end
