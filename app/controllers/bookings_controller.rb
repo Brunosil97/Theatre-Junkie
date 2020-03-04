4c518d108f3cb3e990d42c2017befd971e8f45c2
Bruno Silva <brunosilvinosilva@gmail.com>
2020-03-04 17:23:47 +0000

    Have added controller instances to be able to access them in the view but yet dont work until performance is saved into database

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,16 +9,25 @@ class BookingsController < ApplicationController
     end 
 
     def new 
-
+        @booking = Booking.new
+        @performances = Performance.all 
+        byebug
     end 
 
     def create 
+        @booking = Booking.create(booking_params)
+    end
 
+    def destroy
+        @booking = Booking.find(params[:id])
+        @booking.destroy
+        flash[:notice] = "Booking deleted."
+        redirect_to bookings_path
     end
 
     private 
 
-    def booking_params 
-        # params.require(:booking).permit()
+    def booking_params
+        params.require(:booking).permit(:user_id, :performance_id, :seating_type, :num_of_tickets)
     end 
 end
--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -2,7 +2,7 @@ class PerformancesController < ApplicationController
 
     
     def index 
-
+       @performances = ApiHelper::Api.performance_api(params[:data].to_i)
     end 
 
     def edit 
