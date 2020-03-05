9702270b4842d473dc4dbdc9980304418d23543c
Bruno Silva <brunosilvinosilva@gmail.com>
2020-03-05 15:58:08 +0000

    I have made multiple changes to css, specifically show page of the show. Including if statements to allow Venue API to also be accessed with Event show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,5 +8,12 @@ class EventsController < ApplicationController
     def show
         @shows = ApiHelper::Api.events_api
         @show = @shows.find{|show| show['EventId'] == params[:id].to_i} 
+        if @show
+            @theatres = ApiHelper::Api.venue_api
+            @theatre = @theatres.find {|theatre| theatre['VenueId'] == @show['VenueId']}
+        else
+            flash[:notice] = "Oops someting went wrong with that show"
+            redirect_to events_path
+        end
     end
 end
